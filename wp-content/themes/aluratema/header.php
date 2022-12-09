c4881a61bdafdc0e72c3264f6791d304c04191a9
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-12-09 09:20:56 -0400

    Header completo, estilizado com css

--- wp-content/themes/aluratema/header.php
+++ wp-content/themes/aluratema/header.php
@@ -4,16 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/normalize.css' ?>">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/bootstrap.css' ?>">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/header.css' ?>">
     <title><?php bloginfo('name'); ?></title>
     <? wp_head(); ?>
 </head>
-<body>
-<br><br>
-
-<?php
-the_custom_logo();
-wp_nav_menu(
-    array(
-    'menu' => 'menu-navegacao'
-    )
-);  
+<body <?php body_class(); ?>>
+<header class="site-header">
+    <div class="container-alura">
+        <?php
+        the_custom_logo();
+        ?>
+        <nav>
+            <?php
+            wp_nav_menu(
+                array(
+                    'menu' => 'menu-navegacao',
+                    'menu_id' => 'menu-principal'
+                )
+            );
+            ?>
+        </nav>
+    </div>
+</header>
