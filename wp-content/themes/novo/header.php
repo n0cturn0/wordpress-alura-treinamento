ff826f05fb1aea29b256c0b912c3e2c52fc4a311
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-12-14 07:58:37 -0400

    Adicionado style files

--- wp-content/themes/novo/header.php
+++ wp-content/themes/novo/header.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/bootstrap.css' ?>">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/font.css' ?>">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/style.css' ?>">
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
    
